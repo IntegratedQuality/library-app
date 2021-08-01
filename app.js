d4da080bd707b1fcb2d7770748dda74acce9bc80
GunseiKPaseri <GunseiKPaseri@gmail.com>
2021-08-01 17:53:29 +0900

    initial commit🎉

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT =3000;
+
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT,()=>{
+    console.log(`listen http://localhost:${PORT}`);
+});
