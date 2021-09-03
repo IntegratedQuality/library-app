d0d1a1b3198fbd738504ac0fb53aaf7e30881df4
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-03 15:09:59 +0900

    Fix index.js

---
+++ public/index.js
@@ -0,0 +1,30 @@
+// 蔵書一覧を取得する
+document.addEventListener('onload', async (e) => {
+    e.preventDefault();
+    const start = 0;
+    const URI = `/api/v1/books?${new URLSearchParams({ start })}`;
+    console.log("GET", URI);
+    const t = await fetch(URI);
+    const u = await t.json();
+    
+    // 描画
+    // document.getElementById('getoutput').value = JSON.stringify(u);
+}, false);
+
+// ページ遷移
+for (pageLink of document.getElementsByClassName('page-link')) {
+    const pageNum = parseInt(pageLink.textContent);
+    if (isNaN(pageNum)) {
+        continue;
+    }
+
+    pageLink.addEventListener('click', async (e) => {
+        // const URI = `/api/v1/books?${new URLSearchParams({ offset })}`;
+        // console.log("GET", URI);
+        // const t = await fetch(URI);
+        // const u = await t.json();
+
+        // 描画
+
+    });
+}
