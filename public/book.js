ffece0b0677bbed7a3549aa1a5d4fd7dcb957461
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-04 19:36:08 +0900

    rename book_details.html

--- public/book.js
+++ public/book.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', (e) => {
     const params = new URLSearchParams(document.location.search.substring(1));
-    console.log(params.get(''));
+    console.log(params.get('id'));
 }, false);
