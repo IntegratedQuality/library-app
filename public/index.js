28c16b7cee97773f98e81c174160c3ebb5327064
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-04 16:52:31 +0900

    APIから取得した書籍データの描画処理を実装

--- public/index.js
+++ public/index.js
@@ -1,37 +1,49 @@
-
+/**
+ * TODO
+ * - [ ] 個別ページへのリンクを張る
+ */
 // 蔵書一覧の読み込み
 window.addEventListener('load', async (e) => {
 
     const params = new URLSearchParams(document.location.search.substring(1));
-    const pageNum = parseInt(params.get('page'));
+    let pageNum = parseInt(params.get('page'));
     if (isNaN(pageNum)) {
+        pageNum = 1;
         console.log("ERROR: fail to parse page number (string -> number). Maybe query string is invalid");
     }
-    const start = pageNum * 5;
+    const perPage = 5;
+    const start = (pageNum - 1) * perPage;
 
     // 書籍データの取得
-    // const URI = `/api/v1/books?${new URLSearchParams({ start })}`;
-    // console.log("GET", URI);
-    // const t = await fetch(URI);
-    // const u = await t.json();
+    const URI = `/api/v1/books?${new URLSearchParams({ start })}`;
+    console.log("GET", URI);
+    const t = await fetch(URI);
+    const u = await t.json();
 
     // 書籍リストを描画
-    // console.log(JSON.stringify(u));
+    let booksHTML = '';
+    for (bookData of u.list) {
+        booksHTML += `<li class="list-group-item py-4 px-1"><div class="row"><a href="#" class="text-decoration-none lead pb-2">${bookData.title}</a></div></li>`;
+    }
+    document.getElementById('book-list-area').innerHTML = booksHTML;
 
-    // paginationの挿入
-    maxPage = 10;
+    // paginationを描画
+    maxPage = Math.floor(parseInt(u.total) / perPage) + 1;
+    if (isNaN(maxPage)) {
+        console.log("ERROR: fail to parse page number (string -> number)");
+    }
 
     for (pageArea of document.getElementsByClassName('page-area')) {
 
-        let html = `<ul class="pagination">`;
-        html += `<li class="page-item"><a class="page-link" href="http://localhost:3000/?page=${Math.max(1, pageNum - 1)}"><span aria-hidden="true">&laquo;</span></a></li>`
+        let pageHTML = `<ul class="pagination">`;
+        pageHTML += `<li class="page-item"><a class="page-link" href="http://localhost:3000/?page=${Math.max(1, pageNum - 1)}"><span aria-hidden="true">&laquo;</span></a></li>`
 
-        for (let p = 1; p <= 10; p += 1) {
-            html += `<li class="page-item ${p === pageNum ? 'active' : ''}"><a class="page-link" href="http://localhost:3000/?page=${p.toString()}">${p.toString()}</a></li>`
+        for (let p = 1; p <= maxPage; p += 1) {
+            pageHTML += `<li class="page-item ${p === pageNum ? 'active' : ''}"><a class="page-link" href="http://localhost:3000/?page=${p.toString()}">${p.toString()}</a></li>`
         }
 
-        html += `<li class="page-item"><a class="page-link" href="http://localhost:3000/?page=${Math.min(maxPage, pageNum + 1)}"><span aria-hidden="true">&raquo;</span></a></li>`
-        pageArea.innerHTML = html;
+        pageHTML += `<li class="page-item"><a class="page-link" href="http://localhost:3000/?page=${Math.min(maxPage, pageNum + 1)}"><span aria-hidden="true">&raquo;</span></a></li>`
+        pageArea.innerHTML = pageHTML;
     }
 }, false);
 
