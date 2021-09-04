ce7aaadf0639db564a0843be29733b5cb8543c3c
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-05 00:58:04 +0900

    返却ボタンを追加

---
+++ return.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', async (e) => {
+    // 現在そのユーザが借りている本の一覧を取得する
+
+    // 描画する
+    
+}, false);
+
+document.getElementById('return-button').addEventListener('click', async (e) => {
+    // チェックボックスの内容に基づき、返却する本のIDのリストを取得する
+    
+    // すべてのIDの本を返却する（エラー処理に注意？）
+    
+}, false);
