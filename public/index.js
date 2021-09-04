8ffe8fcfe0dd8c706714b3df241f469e31bbf2f8
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-04 16:16:30 +0900

    paginationを実装

--- public/index.js
+++ public/index.js
@@ -3,33 +3,40 @@
 window.addEventListener('load', async (e) => {
 
     const params = new URLSearchParams(document.location.search.substring(1));
-    const start = params.get('page') * 5;
+    const pageNum = parseInt(params.get('page'));
+    if (isNaN(pageNum)) {
+        console.log("ERROR: fail to parse page number (string -> number). Maybe query string is invalid");
+    }
+    const start = pageNum * 5;
 
-    // 本データの読み込み
-    const URI = `/api/v1/books?${new URLSearchParams({start})}`;
-    console.log("GET", URI);
-    const t = await fetch(URI);
-    const u = await t.json();
-    
-    // 描画
-    console.log(JSON.stringify(u));
-    
+    // 書籍データの取得
+    // const URI = `/api/v1/books?${new URLSearchParams({ start })}`;
+    // console.log("GET", URI);
+    // const t = await fetch(URI);
+    // const u = await t.json();
+
+    // 書籍リストを描画
+    // console.log(JSON.stringify(u));
+
+    // paginationの挿入
+    maxPage = 10;
+
+    for (pageArea of document.getElementsByClassName('page-area')) {
+
+        let html = `<ul class="pagination">`;
+        html += `<li class="page-item"><a class="page-link" href="http://localhost:3000/?page=${Math.max(1, pageNum - 1)}"><span aria-hidden="true">&laquo;</span></a></li>`
+
+        for (let p = 1; p <= 10; p += 1) {
+            html += `<li class="page-item ${p === pageNum ? 'active' : ''}"><a class="page-link" href="http://localhost:3000/?page=${p.toString()}">${p.toString()}</a></li>`
+        }
+
+        html += `<li class="page-item"><a class="page-link" href="http://localhost:3000/?page=${Math.min(maxPage, pageNum + 1)}"><span aria-hidden="true">&raquo;</span></a></li>`
+        pageArea.innerHTML = html;
+    }
 }, false);
 
-// ページ遷移イベントの設定
-for (pageLink of document.getElementsByClassName('page-link')) {
-    const pageNum = parseInt(pageLink.textContent);
-    console.log(pageLink.textContent);
-    // pagenation の実装がわかんな～～い
 
-    pageLink.addEventListener('click', async (e) => {
-        e.preventDefault();
-        // const URI = `/api/v1/books?${new URLSearchParams({ offset })}`;
-        // console.log("GET", URI);
-        // const t = await fetch(URI);
-        // const u = await t.json();
 
-        // 描画
 
-    });
-}
+
+
