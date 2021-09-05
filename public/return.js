ed1a44b68f3c43740e675e416608bbb449eda108
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-05 12:47:28 +0900

    貸出中の資料の一覧を表示できるように

--- public/return.js
+++ public/return.js
@@ -1,6 +1,11 @@
 // TODO: ユーザIDを動的に取得する
+// TODO: えらーはんどりんぐ
+// TODO: ボタンの不可視化
+// MEMO: 書籍データを結合しているところが重そう？
+
 document.addEventListener('DOMContentLoaded', async (e) => {
-    // 現在そのユーザが借りている本の一覧を取得する
+
+    // 現在ユーザが借りている本の一覧を取得する
     const userID = 1;
     const start = 0;
     const URI = `/api/v1/user/${userID}/history?${new URLSearchParams({ start })}`;
@@ -13,8 +18,50 @@ document.addEventListener('DOMContentLoaded', async (e) => {
     const unreturedBooks = u.list.filter(x => x.return_time === null);
     console.log(unreturedBooks);
 
+    // 貸出状態に基づいて描画する
+    const target = document.getElementById('rent-list-area');
+    let html = `<p class="">貸出中の資料：<span class="fw-bold">${unreturedBooks.length}</span>冊</p>`
 
-    // 描画する
+    if (unreturedBooks.length === 0) {
+        html += `<p>現在貸し出し中の資料はありません</p>`
+        target.innerHTML = html;
+        return;
+    }
+
+    html += `<table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">#</th>
+                        <th scope="col">ISBN</th>
+                        <th scope="col">返却期限</th>
+                        <th scope="col">書名</th>
+                    </tr>
+            </thead>`;
+    html += '<tbody>';
+    for (const [i, rentData] of unreturedBooks.entries()) {
+        console.log(i, rentData);
+        // 書籍データの取得
+        const URI = `/api/v1/book/${rentData.book_id}`;
+        console.log("GET", URI);
+        const t = await fetch(URI);
+        const u = await t.json();
+        console.log(u)
+
+        // 取得に失敗したら描画しない
+
+        // 書籍データの描画
+        html += `<tr>
+                    <th scope="col"><input id="table-row-${i}" class="return-checkbox" type="checkbox"></th>
+                    <th scope="col">${i}</th>
+                    <th scope="col">${u.isbn}</th>
+                    <th scope="col">返却期限はないよ！</th>
+                    <th scope="col"><a href="/book.html?id=${u.id}" class="text-decoration-none">${u.title}</a></th>
+                </tr>`;
+    }
+    html += '</tbody>';
+    html += '</table>';
+    target.innerHTML = html;
 
 }, false);
 
