bbfe51b821c7870cf89c4c0fb6014a6a4a45401c
longrun <longrun@DESKTOP-TF7AQG7.localdomain>
2021-09-05 14:33:08 +0900

    返却時の挙動を修正

--- public/return.js
+++ public/return.js
@@ -1,6 +1,5 @@
 // TODO: ユーザIDを動的に取得する
-// TODO: えらーはんどりんぐ
-// TODO: ボタンの不可視化
+// TODO: APIと相談しながら仕様を変えるかも
 // MEMO: 書籍データを結合しているところが重そう？
 
 document.addEventListener('DOMContentLoaded', async (e) => {
@@ -13,12 +12,10 @@ document.addEventListener('DOMContentLoaded', async (e) => {
     console.log('GET', URI)
     const t = await fetch(URI);
     const u = await t.json();
-
     console.log(u);
-    const unreturedBooks = u.list.filter(x => x.return_time === null);
-    console.log(unreturedBooks);
-
+    
     // 貸出状態に基づいて描画する
+    const unreturedBooks = u.list.filter(x => x.return_time === null);
     const target = document.getElementById('rent-list-area');
     let html = `<p class="">貸出中の資料：<span class="fw-bold">${unreturedBooks.length}</span>冊</p>`
 
@@ -42,23 +39,26 @@ document.addEventListener('DOMContentLoaded', async (e) => {
             </thead>`;
     html += '<tbody>';
     for (const [i, rentData] of unreturedBooks.entries()) {
-        console.log(i, rentData);
         // 書籍データの取得
         const URI = `/api/v1/book/${rentData.book_id}`;
         console.log("GET", URI);
         const t = await fetch(URI);
-        const u = await t.json();
-        console.log(u)
 
         // 取得に失敗したら描画しない
+        if (t.status >= 400) {
+            console.error(`ERROR: cannot get book data.`);
+            continue;
+        }
+        const bookData = await t.json();
+        console.log(bookData)
 
         // 書籍データの描画
         html += `<tr>
-                    <td><input id="table-row-${i}" class="return-checkbox" type="checkbox"></th>
+                    <td><input name="${bookData.id}" class="return-checkbox" type="checkbox"></th>
                     <td>${i}</th>
-                    <td>${u.isbn}</th>
+                    <td>${bookData.isbn}</th>
                     <td>yyyy-mm-dd</th>
-                    <td><a href="/book.html?id=${u.id}" class="text-decoration-none">${u.title}</a></th>
+                    <td><a href="/book.html?id=${bookData.id}" class="text-decoration-none">${bookData.title}</a></th>
                 </tr>`;
     }
     html += '</tbody>';
@@ -69,7 +69,25 @@ document.addEventListener('DOMContentLoaded', async (e) => {
 
 document.getElementById('return-button').addEventListener('click', async (e) => {
     // チェックボックスの内容に基づき、返却する本のIDのリストを取得する
+    const bookIDList = [];
+    for (let box of document.getElementsByClassName('return-checkbox')) {
+        if (box.checked) {
+            bookIDList.push(box.name);
+        }
+    }
 
-    // すべてのIDの本を返却する（エラー処理に注意？）
+    // すべてのIDの本を返却する
+    for (let bookID of bookIDList) {
+        const URI = `/api/v1/book/${bookID}/return`;
+        console.log('POST', URI);
+        const t = await fetch(URI, { method: "POST" });
+        if (t.status !== 204) {
+            console.log(`ERROR: HTTP responce status is invalid.\n Expected 204 but ${t.status}.\n Fail to return book (id=${bookID}). Maybe wrong bookID is sent.`);
+            continue;
+        }
+    }
+
+    // デバッグするときは消そう！
+    document.location.reload();
 
 }, false);
